ac3e0056e6bfe886a9a4d6c58df7ab85daa98b8d
Alexis0512 <alexistlc@orange.fr>
2025-01-23 12:09:27 +0100

    upload de fichier

--- backend/src/fichiers/fichiers.module.ts
+++ backend/src/fichiers/fichiers.module.ts
@@ -1,12 +1,21 @@
+// fichiers.module.ts
 import { Module } from '@nestjs/common';
-import { FichiersController } from './fichiers.controller';
 import { FichiersService } from './fichiers.service';
+import { FichiersController } from './fichiers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FichierEntity } from 'src/entities/fichier.entity';
+import { ProjetEntity } from 'src/entities/projet.entity';
+import { UtilisateurEntity } from 'src/entities/utilisateur.entity'; // Assurez-vous d'importer l'entité utilisateur
+import { ProjetModule } from 'src/projet/projet.module'; // Si nécessaire, importer ProjetsModule
+import { UtilisateursModule } from 'src/utilisateurs/utilisateurs.module'; // Importez UtilisateursModule
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([FichierEntity, ProjetEntity, UtilisateurEntity]), // Assurez-vous d'ajouter UtilisateurEntity ici
+    ProjetModule, // Importer ProjetsModule si nécessaire
+    UtilisateursModule, // Importer UtilisateursModule pour résoudre la dépendance UtilisateurEntityRepository
+  ],
   controllers: [FichiersController],
   providers: [FichiersService],
-  imports: [TypeOrmModule.forFeature([FichierEntity])],  // Déclarez FichierEntity ici
 })
 export class FichiersModule {}
