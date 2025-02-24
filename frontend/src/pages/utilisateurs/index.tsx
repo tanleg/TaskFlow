5dd8b340f84aa1c44ce6c13e2c2b6820b6a73a4b
Riad Sacroud <sacroudr@gmail.com>
2025-02-24 14:24:38 +0100

    page utilisateurs

--- frontend/src/pages/utilisateurs/index.tsx
+++ frontend/src/pages/utilisateurs/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Box } from "@mui/material";
+import ListeUtilisateurs from "../../views/utilisateurs/listeUtiseurs";
 const Users: React.FC = () => {
   return  (
     <Box sx={{ marginLeft: "290px", padding: "20px" }}>
-      <h1>Bienvenue sur la page utilisateur</h1>
+      <ListeUtilisateurs />
     </Box>
 )
 };
