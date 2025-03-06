fba28396b37d036e463b7bd61c9218ef871e812a
Alexis0512 <alexistlc@orange.fr>
2025-03-06 18:25:28 +0100

    fin commentaire backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,12 +7,12 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    // origin: '*', // Origine autorisée
+    
     origin: ['http://localhost:5173', 'http://192.168.1.10:5173', 'http://192.168.41.238:5173'], // Origine autorisée
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Méthodes autorisées
     credentials: true, // Autorise les cookies ou l'authentification avec session
   });
-  
+  //L'application écoute sur le port 3000
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
